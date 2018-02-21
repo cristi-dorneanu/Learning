51bb915d9833137b422ac76c487ed755c0b5ca8a
cristi-dorneanu <dorneanucristian95.gmail.com>
2018-02-21 18:28:48 +0200

    started solving first exercises from murach book

---
+++ Practice/servlet_and_jsp/eclipse/book_apps/ch02email/src/main/java/murach/data/UserDB.java
@@ -0,0 +1,12 @@
+package main.java.murach.data;
+
+import main.java.murach.business.User;
+
+public class UserDB {
+
+    public static long insert(User user) {
+        // TODO: Add code that adds the user to the database
+        // NOTE: This is shown in chapters 11-13
+        return 0;
+    }
+}
---
+++ Practice/servlet_and_jsp/eclipse/ex_starts/testApp/src/java/murach/home/TestServlet.java
@@ -0,0 +1,13 @@
+package java.murach.home;
+
+import javax.servlet.http.HttpServlet;
+
+public class TestServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3243066530750653604L;
+
+	
+}
